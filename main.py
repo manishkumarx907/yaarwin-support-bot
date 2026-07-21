3cf599a0a1f8daef8900074ccd8b59d10801caa3
manishkumarx907 <manishkumarku04@gmail.com>
2026-07-21 22:10:55 +0530

    Upgrade model version from 1.5 to 2.0

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ GEMINI_KEY = os.getenv("GEMINI_API_KEY")
 # Gemini setup
 genai.configure(api_key=GEMINI_KEY)
 
-model = genai.GenerativeModel("gemini-1.5-flash")
+model = genai.GenerativeModel("gemini-2.0-flash")
 
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
