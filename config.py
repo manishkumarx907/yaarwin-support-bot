09da43c108bfcdcabb3cf0695ff6f8da5b5794c5
manishkumarx907 <manishkumarku04@gmail.com>
2026-07-21 12:45:20 +0530

    Add config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+BOT_TOKEN = "8907587670:AAEWj0GM9ngYuNmliBel3qCXIlPo01TBt1o"
+
+REGISTER_LINK = "https://www.yaarwin.online/#/register?invitationCode=182763900728"
+
+SUPPORT_USERNAME = "https://t.me/Vpnusern"
+
+BOT_NAME = "YAAR WIN SUPPORT"
+
+ADMIN_ID = 123456789
